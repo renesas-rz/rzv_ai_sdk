b4c00bdded211589ff824736c51dc8e5928f2e9e
wkurata <wakako.kurata.px@renesas.com>
2023-04-11 17:26:27 +0900

    Added object_detection

---
+++ R01_object_detection/src/build/CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/MeraDrpRuntimeWrapper.cpp.o"
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/PreRuntime.cpp.o"
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/ascii.cpp.o"
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/box.cpp.o"
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/camera.cpp.o"
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/image.cpp.o"
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/main.cpp.o"
+  "CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/wayland.cpp.o"
+  "sample_app_drpai_tvm_yolov3_coralcam_hdmi"
+  "sample_app_drpai_tvm_yolov3_coralcam_hdmi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_app_drpai_tvm_yolov3_coralcam_hdmi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
