f769d20c4565f185edc7db4463fc74ba2fc3439f
enkrateia007 <alokranjan.swain@quest-global.com>
2023-06-08 15:30:47 +0900

    Initial commit

---
+++ Q05_suspicious_activity/readme.md
@@ -0,0 +1,221 @@
+# Suspicious Activity Classification Application
+
+## Application: Overview
+The susspicious  activity classification application allows to classify between Violence and Non-Violence from video input using a custom AI model which has a CNN module for feature extraction and MLP for aggregating features.
+
+It has 2 modes of running.
+
+3. Using Video as input
+4. Using Camera as Input
+
+#### Demo Video:
+The Demo videos for the susspicious  activity classification application can be found at
+
+
+## Application: Requirements
+
+#### Hardware Requirements
+- RZ/V2L Evaluation Board Kit
+    - Coral Camera
+- USB Mouse
+- USB Keyboard
+- USB Hub
+- HDMI monitor & Micro HDMI Cable
+- Ethernet Cable
+
+[Details](https://github.com/renesas-rz/rzv_ai_sdk/blob/main/README.md)
+
+#### Software Requirements for building the application
+- Ubuntu 20.04
+- OpenCV 4.x
+- C++11 or higher
+
+	
+## Application: Build Stage
+
+>**Note:** User can skip to the next stage (deploy) if they don't want to build the application. All pre-built binaries are provided.
+
+**Note:** This project expects the user to have completed [Getting Startup Guide](../README.md#startup-guide) provided by Renesas. 
+
+After completion of the guide, the user is expected of following things.
+- The Board Set Up and booted. 
+- SD Card Prepared 
+- The docker image amd container for `rzv2l_ai_sdk_image` running on host machine.
+
+>**Note:** Docker container is required for building the sample application. By default the Renesas will provide the container named as `rzv2l_ai_sdk_container`. Please use the docker container name as assigned by the user when building the container.
+
+#### Application: File Generation
+
+1. Copy the repository from the GitHub to the desired location. 
+    1. It is recommended to copy/clone the repository on the `data` folder which is mounted on the `rzv2l_ai_sdk_container` docker container. 
+    ```sh
+    cd <path_to_data_folder_on_host>
+    git clone https://github.com/renesas-rz/rzv_ai_sdk.git
+    ```
+   >Note: Please verify the git repository url if error occurs.
+2. Run the docker container and open the bash terminal on the container.
+
+> Note: All the build steps/commands listed below are executed on the docker container terminal.
+
+3. Assign path to the `data` directory mounted on the `rzv2l_ai_sdk_container` docker container
+
+```sh
+export PROJECT_PATH=/drp-ai_tvm/data/
+```
+
+4. Go to the `src` directory of the application
+
+```sh
+cd ${PROJECT_PATH}/rzv_ai_sdk/Q05_suspicious_activity/suspicious_activity/src/
+```
+>**Note:**`rzv_ai_sdk` is the repository name corresponding to the cloned repository. Please verify the repository name if error occurs.
+5. Build the application on docker environment by following the steps below
+
+```sh
+mkdir -p build && cd build
+```
+```sh
+cmake -DCMAKE_TOOLCHAIN_FILE=./toolchain/runtime.cmake ..
+```
+```sh
+make -j$(nproc)
+```
+
+The following application file would be generated in the `src/build` directory
+- suspicious_activity
+
+
+## Application: Deploy Stage
+
+#### Mode: Board Deployment
+##### Folder Structure in the board
+```sh
+/
+└── home
+    └── root
+        └── tvm
+            ├── cnn_module
+            │   ├── deploy.json
+            │   ├── deploy.params
+            │   └── deploy.so
+            │   
+            ├── mlp_module
+            │   ├── deploy.json
+            │   ├── deploy.params
+            │   └── deploy.so
+            └── suspicious_activity
+
+```
+
+
+## Application: Runtime Stage
+
+##### Mode: Camera Input
+- The application takes input from MIPI Coral Camera.
+- Run the MIPI camera script, placed in `/home/root/` directory
+```sh
+./v4l2-init.sh
+```
+> Note: The output resolution depends on the input camera resolution, which could be modified from the script, before running it. Default resolution:1920x1080
+```sh 
+./suspicious_activity CAMERA 
+```
+
+##### Mode: Video Input
+
+```sh 
+./suspicious_activity VIDEO <video_file_path>
+```
+> Note: Tested with video file format `.mp4` and `.avi`.
+
+
+- Press `Esc` key to terminate the application.
+
+
+## Application: Specifications
+
+#### Model Details
+
+
+##### CNN Module
+
+==========================================================================================
+Layer (type:depth-idx)                   Output Shape              Param #
+==========================================================================================
+├─Sequential: 1-1                        [-1, 512, 7, 7]           --
+|    └─Conv2d: 2-1                       [-1, 64, 112, 112]        9,408
+|    └─BatchNorm2d: 2-2                  [-1, 64, 112, 112]        128
+|    └─ReLU: 2-3                         [-1, 64, 112, 112]        --
+|    └─MaxPool2d: 2-4                    [-1, 64, 56, 56]          --
+|    └─Sequential: 2-5                   [-1, 64, 56, 56]          --
+|    |    └─BasicBlock: 3-1              [-1, 64, 56, 56]          73,984
+|    |    └─BasicBlock: 3-2              [-1, 64, 56, 56]          73,984
+|    |    └─BasicBlock: 3-3              [-1, 64, 56, 56]          73,984
+|    └─Sequential: 2-6                   [-1, 128, 28, 28]         --
+|    |    └─BasicBlock: 3-4              [-1, 128, 28, 28]         230,144
+|    |    └─BasicBlock: 3-5              [-1, 128, 28, 28]         295,424
+|    |    └─BasicBlock: 3-6              [-1, 128, 28, 28]         295,424
+|    |    └─BasicBlock: 3-7              [-1, 128, 28, 28]         295,424
+|    └─Sequential: 2-7                   [-1, 256, 14, 14]         --
+|    |    └─BasicBlock: 3-8              [-1, 256, 14, 14]         919,040
+|    |    └─BasicBlock: 3-9              [-1, 256, 14, 14]         1,180,672
+|    |    └─BasicBlock: 3-10             [-1, 256, 14, 14]         1,180,672
+|    |    └─BasicBlock: 3-11             [-1, 256, 14, 14]         1,180,672
+|    |    └─BasicBlock: 3-12             [-1, 256, 14, 14]         1,180,672
+|    |    └─BasicBlock: 3-13             [-1, 256, 14, 14]         1,180,672
+|    └─Sequential: 2-8                   [-1, 512, 7, 7]           --
+|    |    └─BasicBlock: 3-14             [-1, 512, 7, 7]           3,673,088
+|    |    └─BasicBlock: 3-15             [-1, 512, 7, 7]           4,720,640
+|    |    └─BasicBlock: 3-16             [-1, 512, 7, 7]           4,720,640
+├─Sequential: 1-2                        [-1, 31]                  --
+|    └─AdaptiveConcatPool2d: 2-9         [-1, 1024, 1, 1]          --
+|    |    └─AdaptiveMaxPool2d: 3-17      [-1, 512, 1, 1]           --
+|    |    └─AdaptiveAvgPool2d: 3-18      [-1, 512, 1, 1]           --
+|    └─Flatten: 2-10                     [-1, 1024]                --
+|    └─BatchNorm1d: 2-11                 [-1, 1024]                2,048
+|    └─Dropout: 2-12                     [-1, 1024]                --
+|    └─Linear: 2-13                      [-1, 512]                 524,288
+|    └─ReLU: 2-14                        [-1, 512]                 --
+|    └─BatchNorm1d: 2-15                 [-1, 512]                 1,024
+|    └─Dropout: 2-16                     [-1, 512]                 --
+|    └─Linear: 2-17                      [-1, 31]                  15,872
+==========================================================================================
+Total params: 21,827,904
+Trainable params: 21,827,904
+Non-trainable params: 0
+Total mult-adds (G): 3.71
+==========================================================================================
+Input size (MB): 0.57
+Forward/backward pass size (MB): 54.38
+Params size (MB): 83.27
+Estimated Total Size (MB): 138.21
+
+##### MLP Module
+
+==========================================================================================
+Layer (type:depth-idx)                   Output Shape              Param #
+==========================================================================================
+├─Linear: 1-1                            [-1, 1, 128]              1,280,128
+├─ReLU: 1-2                              [-1, 1, 128]              --
+├─Linear: 1-3                            [-1, 1, 1]                129
+├─Sigmoid: 1-4                           [-1, 1, 1]                --
+==========================================================================================
+Total params: 1,280,257
+Trainable params: 1,280,257
+Non-trainable params: 0
+Total mult-adds (M): 1.28
+==========================================================================================
+Input size (MB): 0.04
+Forward/backward pass size (MB): 0.00
+Params size (MB): 4.88
+Estimated Total Size (MB): 4.92
+
+
+#### Dataset 
+Dataset Contains 1000 Violence and 1000 non-violence videos collected from youtube videos.
+[Dataset-Link](https://www.kaggle.com/datasets/mohamedmustafa/real-life-violence-situations-dataset)
+#### AI Inference time
+Total AI inference time (Pre-processing + AI model inference) - 
+| Training Accuracy   |Validation Accuracy   |  Testing Accuracy |
+|---|---|---|
+
---
+++ Q05_suspicious_activity/src/suspicious_activity.cpp
@@ -0,0 +1,434 @@
+/*
+ * Original Code (C) Copyright Edgecortix, Inc. 2022
+ * Modified Code (C) Copyright Renesas Electronics Corporation 2023
+ *　
+ *  *1 DRP-AI TVM is powered by EdgeCortix MERA™ Compiler Framework.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+/***********************************************************************************************************************
+ * File Name    : fish_classification.cpp
+ * Version      : 1.0
+ * Description  : RZ/V2L SDK AI Application: Fish Classification
+ ***********************************************************************************************************************/
+/*****************************************
+ * includes
+ *****************************************/
+#include <builtin_fp16.h>
+#include <fstream>
+#include <string>
+#include <sys/time.h>
+#include <climits>
+#include <cstdlib>
+#include <thread>
+#include <cstring>
+#include "MeraDrpRuntimeWrapper.h"
+#include "opencv2/core.hpp"
+#include "iostream"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/plot.hpp"
+#include <vector>
+#include <glob.h>
+#include <cmath>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <queue>
+
+#define GREEN cv::Scalar(0, 255, 0)
+#define RED cv::Scalar(0, 0, 255)
+#define WHITE cv::Scalar( 255, 255, 255)
+
+#define MODEL_IN_H (128)
+#define MODEL_IN_W (128)
+#define MODEL_IN_C (3)
+
+bool running_process_frame     = true;
+
+int font_size   = 1;
+int font_weight = 2;
+int wait_key    = 0;
+int g_count     = 0;
+
+float threshold = 0.5;
+float val;
+double count    = 0;
+unsigned int out;
+/* Default frame interval */
+unsigned int FRAME_INTERVAL = 4; 
+/* Default buffer size */
+unsigned int BUFFER_SIZE    = 5;
+
+std::vector<double> x;
+std::vector<double> y;
+std::queue<cv::Mat> frame_queue;
+
+const std::string non_violence      = "Non Violence";
+const std::string violence          = "Violence";
+const std::string none              = "None";
+std::string result                  = none;
+
+
+const int BATCH_SIZE = 10;
+cv::VideoCapture cap;
+cv::Mat frame;
+
+uint32_t out_size_arr;
+std::vector<float> floatarr(1);
+
+MeraDrpRuntimeWrapper embedding_model;
+MeraDrpRuntimeWrapper prediction_model;
+
+cv::Mat hwc2chw(const cv::Mat &image);
+cv::Mat run_inference(cv::Mat frame);
+cv::Scalar Color;
+
+/*****************************************
+ * Function Name     : float16_to_float32
+ * Description       : Function by Edgecortex. Cast uint16_t a into float value.
+ * Arguments         : a = uint16_t number
+ * Return value      : float = float32 number
+ ******************************************/
+float float16_to_float32(uint16_t a)
+{
+    return __extendXfYf2__<uint16_t, uint16_t, 10, float, uint32_t, 23>(a);
+}
+/*****************************************
+ * Function Name : hwc2chw
+ * Description   : This function takes an input image in HWC (height, width, channels)
+ *                 format and returns an output image in CHW (channels, height, width) format.
+ * Arguments     : image = cv::Mat
+ ******************************************/
+cv::Mat hwc2chw(const cv::Mat &image)
+{
+    std::vector<cv::Mat> rgb_images;
+    cv::split(image, rgb_images);
+    cv::Mat m_flat_r = rgb_images[0].reshape(1, 1);
+    cv::Mat m_flat_g = rgb_images[1].reshape(1, 1);
+    cv::Mat m_flat_b = rgb_images[2].reshape(1, 1);
+    cv::Mat matArray[] = {m_flat_r, m_flat_g, m_flat_b};
+    cv::Mat flat_image;
+    cv::hconcat(matArray, MODEL_IN_C, flat_image);
+    return flat_image;
+}
+/*****************************************
+ * Function Name : camera_thread
+ * Description   : function to show the ouput camera result
+ ******************************************/
+void camera_thread(void)
+{
+    int frame_count = 0;
+    cv::Point text_position(40, 80);
+    /*start capturing frames*/
+    while (true)
+    {
+        frame_count++;
+        cv::Mat frame;
+        cap.read(frame);
+        /*check frame interval based on the FRAME_INTERVAL parameter*/
+        if (frame_count % FRAME_INTERVAL != 0)
+        {
+            cv::resize(frame, frame, cv::Size(640, 480), cv::INTER_LINEAR);
+            if (result != none)
+            {
+                Color = (result == violence) ? RED : GREEN;
+                putText(frame, result, text_position, cv::FONT_HERSHEY_COMPLEX, font_size, Color, font_weight);
+            }
+            /*display frame*/
+            cv::imshow("Camera", frame);
+            wait_key = cv::waitKey(30);
+            if (wait_key == 27)
+            {
+                running_process_frame = false;
+                break;
+            }
+            continue;
+        }
+        else
+        {
+            /*push frames to the queue for processing if queue size is not 100*/
+            if (frame_queue.size()<100)
+            frame_queue.push(frame);
+            frame_count = 0;
+        }
+    }
+    cap.release();
+    cv::destroyAllWindows();
+}
+/******************************************
+ * Function Name : plot_graph
+ * Description   : function to plot graph with respect to the threshold value.
+ * Arguments     : float value , output threshold value.
+ ******************************************/
+void plot_graph(float value)
+{
+    count++;
+    g_count++;
+    /* push back the count and threshold value to vector array */
+    x.push_back(count);
+    y.push_back(value);
+
+    if(x.size()> 20 || y.size()> 20)
+    {
+        x.erase(x.begin(),x.begin()+1);
+        y.erase(y.begin(),y.begin()+1);
+    }
+
+    cv::Mat1d xData(x);
+    cv::Mat1d yData(y);
+    /* Set text position on graph*/
+    cv::Point text_position(40, 80);
+    cv::Ptr<cv::plot::Plot2d> plot = cv::plot::Plot2d::create(xData,yData);
+    
+    plot->setPlotSize(640,480);
+    plot->setMaxY(2);
+    plot->setMinY(-2);
+    plot->setMaxX(count+1);
+
+    /* Plot graph*/
+    cv::Mat display;
+    plot->render(display);
+    cv::flip(display,display,0);
+    cv::Rect roi(0,0,640,431);
+    display = display(roi);
+    Color = (value >= 0.5) ? GREEN : RED;
+    putText(display,"Threshold : "+ std::to_string(value), text_position, cv::FONT_HERSHEY_COMPLEX, font_size, Color, font_weight);
+    putText(display,"1",cv::Point(10,119), cv::FONT_HERSHEY_COMPLEX,0.65,WHITE,1);
+    putText(display,"0.5",cv::Point(10,183), cv::FONT_HERSHEY_COMPLEX,0.65,WHITE,1);
+    /* show result on graph */
+    cv::imshow("Graph Plot",display );
+    cv::waitKey(30);
+    return;
+}
+/*****************************************
+ * Function Name     : start_runtime
+ * Description       : Function to perform the pre processing and post processing.
+ * Arguments         : *input = frame input address
+ * Return value      : *float floatarr = pre-processed and post-processed output
+ ******************************************/
+void start_runtime(bool flag,float *input)
+{
+    MeraDrpRuntimeWrapper model_runtime = (flag == true)?embedding_model:prediction_model;
+    /*Set Pre-processing output to be inference input. */
+    model_runtime.SetInput(0, input);
+    model_runtime.Run();
+    /* Get the number of output.  */
+    auto output_num = model_runtime.GetNumOutput();
+    if (output_num != 1)
+    {
+        std::cout << "[ERROR] Output size : not 1." << std::endl;
+        abort();
+    }
+    /* get output buffer */
+    auto output_buffer = model_runtime.GetOutput(0);
+    out_size_arr = std::get<2>(output_buffer);
+    floatarr.resize(out_size_arr);
+    /* Post-processing for FP16 */
+    if (InOutDataType::FLOAT16 == std::get<0>(output_buffer))
+    {
+        std::cout << "Output data type : FP16.\n";
+        /* Extract data in FP16 <uint16_t>. */
+        uint16_t *data_ptr = reinterpret_cast<uint16_t *>(std::get<1>(output_buffer));
+        for (int n = 0; n < out_size_arr; n++)
+        {
+            if(flag == true)
+            {
+                /* Cast FP16 output data to FP32. */
+                floatarr[n] = float16_to_float32(data_ptr[n]);
+                std::cout<<"\n\nfloatarr["<<n<<"]: "<<floatarr[n]<<"\n";
+            }
+            else
+            {
+                /* Cast FP16 output data to FP32. */
+                auto prediction = float16_to_float32(data_ptr[n]);
+                std::cout << "\n\nprediction: " << prediction << "\n";
+                
+                if (prediction >= threshold)
+                    result = non_violence;
+                else
+                    result = violence;
+                val = (float)prediction;
+                plot_graph(val);
+            }
+        }
+    }
+    else
+    {
+        std::cout << "[ERROR] Output data type : not floating point." << std::endl;
+        abort();
+    }
+}
+/*****************************************
+ * Function Name : run_inference
+ * Description   : frame preprocessing and postprocessing.
+ * Arguments     : Mat frame
+ * Return value  : Mat mat
+ ******************************************/
+cv::Mat run_inference(cv::Mat frame)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    cv::Size size(MODEL_IN_H, MODEL_IN_W);
+    /*resize the image to the model input size*/
+    cv::resize(frame, frame, size);
+    /*convert to FP32*/
+    frame.convertTo(frame, CV_32FC3);
+    divide(frame, 255.0, frame);
+    cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+    frame = hwc2chw(frame);
+    /*deep copy, if not continuous*/
+    if (!frame.isContinuous())
+        frame = frame.clone();
+    /*start inference using drp runtime*/
+    start_runtime(true,frame.ptr<float>());
+    cv::Mat mat(out_size_arr, 1, CV_32FC1, floatarr.data());
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+    std::cout << "\nAI-Inference Time(ms): " << duration << " ms\n";
+    return mat;   
+}
+/*****************************************
+ * Function Name : process_frames
+ * Description   : frame preprocessing and postprocessing.
+ ******************************************/
+void process_frames(void)
+{
+    std::vector<cv::Mat> frames;
+    std::vector<cv::Mat> features;
+    cv::Point text_position(40, 80);
+
+    /* Model Binary */
+    std::string model_dir = "cnn_module";
+    /* Model Binary */
+    std::string mlp_model = "mlp_module";
+
+    /* Load model_dir structure and its weight to model_runtime object */
+    embedding_model.LoadModel(model_dir);
+    /* Load model_dir structure and its weight to model_runtime object */
+    prediction_model.LoadModel(mlp_model);
+
+    std::cout << "\n[INFO]loaded CNN model:" << model_dir << "\n";
+    std::cout << "\n[INFO]loaded MLP model:" << mlp_model << "\n\n";
+
+    while (running_process_frame)
+    {
+        /* checking input frame array is empty or not */
+        if (frame_queue.empty())
+            continue;
+        cv::Mat frame = frame_queue.front();
+        frame_queue.pop();
+        /* checking the input frame is empty or not*/
+        if(frame.empty())
+        {
+            std::cout << "[ERROR]Unable to read frame" << std::endl;
+            break;
+        }
+        /* run inference*/
+        cv::Mat feature_vector = run_inference(frame);
+        features.push_back(feature_vector);
+        if (features.size() >= BATCH_SIZE)
+        {
+            cv::Mat combined_features;
+            /* vertical concatenation */
+            vconcat(features, combined_features);
+            start_runtime(false,combined_features.ptr<float>());
+            features.erase(features.begin(), features.begin() + BUFFER_SIZE);
+        }
+        cv::resize(frame, frame, cv::Size(640, 480), cv::INTER_LINEAR);
+        if (result != none)
+        {
+            Color = (result == violence) ? RED : GREEN;
+            putText(frame, result, text_position, cv::FONT_HERSHEY_COMPLEX, font_size, Color, font_weight);
+        }
+    }
+    return;
+}
+/******************************************
+ * Function Name : capture_frame
+ * Description   : function to open camera or video source with respect to the source pipeline.
+ * Arguments     : string cap_pipeline input source pipeline
+ ******************************************/
+void capture_frame(std::string cap_pipeline)
+{
+    std::cout << "cap pipeline" << cap_pipeline << "\n";
+    /* Capture stream of frames from camera using Gstreamer pipeline */
+    cap.open(cap_pipeline, cv::CAP_GSTREAMER);
+    if (!cap.isOpened())
+    {
+        /* This section prompt an error message if no video stream is found */
+        std::cout << "[ERROR] Error opening video stream or camera !\n" << std::endl;
+        return;
+    }
+    else
+    {
+        /* Start thread camera_start*/ 
+        std::thread camera_start(camera_thread);
+        process_frames();
+        /* Wait for camera_start finish */ 
+        camera_start.join();
+    }
+    return;
+}
+
+int main(int argc, char **argv)
+{   
+    /* Get input Source WS/VIDEO/CAMERA */
+    std::string input_source = argv[1];
+    /* Fetching Frame interval & Buffer size from arguments*/
+    if (!(argv[2]))
+    {
+        std::cout << "[INFO]Default Frame interval: " << FRAME_INTERVAL << "\n";
+        std::cout << "[INFO]Default Buffer size: " << BUFFER_SIZE << "\n";
+    }
+    else
+    {
+        FRAME_INTERVAL = atoi(argv[2]);
+        std::cout << "[INFO]Frame interval: " << FRAME_INTERVAL << "\n";
+        if (!(argv[3]))
+            std::cout << "[INFO]Default Buffer size: " << BUFFER_SIZE << "\n";
+        else
+        {
+            BUFFER_SIZE = atoi(argv[3]);
+            std::cout << "[INFO]Buffer size: " << BUFFER_SIZE << "\n";
+        }
+    }
+    /* Input source : CAMERA */
+    if (input_source == "CAMERA")
+    {
+        std::cout << "\n[INFO]CAMERA_mode\n";
+        std::string gstreamer_pipeline = "v4l2src device=/dev/video0 ! videoconvert ! appsink";
+        capture_frame(gstreamer_pipeline);
+    }
+    /* Input source : VIDEO */
+    else
+    {
+        std::cout << "\n[INFO] VIDEO_mode\n";
+        capture_frame(argv[1]);
+    }
+    return 0;
+}
