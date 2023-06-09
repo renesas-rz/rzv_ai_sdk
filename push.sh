352c768c7cbb16afcdc5f704c09117da4e06c954
wkurata <wakako.kurata.px@renesas.com>
2023-06-09 18:43:48 +0900

    multi-versioning

---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VERSION=$1
+if [ $VERSION = "" ]; then
+  echo "ERROR: please type version. e.g. ./push.sh 1.00"
+  exit 1
+fi
+
+sed -i -e  "s/version: [0-9]*\.[0-9][0-9]/version: ${VERSION}/g" ./docs/_config.yml 
+sed -i -e  "s/baseurl: \/rzv_ai_sdk\/[0-9]*\.[0-9][0-9]/baseurl: \/rzv_ai_sdk\/${VERSION}/g" ./docs/_config.yml 
+
+cd docs
+bundle exec jekyll build
+cd ..
+
+# git checkout origin/
+sed -i -e  "s/RELEASE_VERSION=[0-9]*\.[0-9][0-9]/RELEASE_VERSION=${VERSION}/g" ./.github/workflows/deploy-version.yml 
+git add .
+git commit -m "${VERSION}"
+git push origin docs
