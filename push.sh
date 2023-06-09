4525ee721643ee7ef7692dd967383442a1a4f3bb
wkurata <wakako.kurata.px@renesas.com>
2023-06-09 19:28:50 +0900

    1.00

--- push.sh
+++ push.sh
@@ -17,4 +17,6 @@ cd ..
 sed -i -e  "s/RELEASE_VERSION=[0-9]*\.[0-9][0-9]/RELEASE_VERSION=${VERSION}/g" ./.github/workflows/deploy-version.yml 
 git add .
 git commit -m "${VERSION}"
+git fetch
+git pull origin docs
 git push origin docs
