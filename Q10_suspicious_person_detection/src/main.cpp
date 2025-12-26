774d79a3ac8ca48e261c348a8ffc91488c71236b
wk-mnA <wakako.kurata.px@renesas.com>
2025-12-26 20:13:29 +0900

    　AI Apps v6.20

--- Q10_suspicious_person_detection/src/main.cpp
+++ Q10_suspicious_person_detection/src/main.cpp
@@ -49,7 +49,6 @@
 #include "define.h"
 /*box drawing*/
 #include "box.h"
-#include "utils.h"
 
 #define SUSPICIOUS  "suspicious"
 
@@ -68,7 +67,6 @@ std::map<std::string, int> input_source_map =
 static sem_t terminate_req_sem;
 static pthread_t ai_inf_thread;
 static pthread_t capture_thread;
-static pthread_t exit_thread;
 static pthread_t kbhit_thread;
 static std::mutex mtx;
 
@@ -97,7 +95,6 @@ cv::Mat input_image;
 std::unordered_map<std::string, std::unordered_map<std::string, std::string>> ini_values;
 std::vector<double> anchors;
 
-bool doubleClick = false;
 #ifdef V2H
     /*DRP-AI Frequency setting*/
     static int32_t drpai_freq;
@@ -162,7 +159,7 @@ std::string float_to_string(float number, int precision = 2)
     stream << std::fixed << number;  
     return stream.str();
 }
-
+ 
 /*****************************************
 * Function Name     : config_read
 * Description       : Read configuration from the config.ini file.
@@ -481,65 +478,6 @@ void R_Post_Proc(float* floatarr)
     filter_boxes_nms(det, det.size(), TH_NMS);
     mtx.unlock();
     return ;
-}
-
-/*****************************************
- * Function Name : R_exit_Thread
- * Description   : Executes the double click exit thread
- * Arguments     : threadid = thread identification
- * Return value  : -
- ******************************************/
-void *R_exit_Thread(void *threadid)
-{
-    /*Semaphore Variable*/
-    int32_t kh_sem_check = 0;
-
-    /*Variable for checking return value*/
-    int8_t ret = 0;
-    devices dev;
-
-    /*Set Standard Input to Non Blocking*/
-    errno = 0;
-    ret = fcntl(0, F_SETFL, O_NONBLOCK);
-    if (-1 == ret)
-    {
-        fprintf(stderr, "[ERROR] Failed to run fctnl(): errno=%d\n", errno);
-        goto err;
-    }
-
-    while (1)
-    {
-        /*Gets the Termination request semaphore value. If different then 1 Termination was requested*/
-        /*Checks if sem_getvalue is executed wihtout issue*/
-        errno = 0;
-        ret = sem_getvalue(&terminate_req_sem, &kh_sem_check);
-        if (0 != ret)
-        {
-            fprintf(stderr, "[ERROR] Failed to get Semaphore Value: errno=%d\n", errno);
-            goto err;
-        }
-        /*Checks the semaphore value*/
-        if (1 != kh_sem_check)
-        {
-            goto exit_end;
-        }
-
-        dev.detect_mouse_click();
-        if (doubleClick)
-        {
-            goto err;
-        }
-    }
-
-/*Error Processing*/
-err:
-    /*Set Termination Request Semaphore to 0*/
-    sem_trywait(&terminate_req_sem);
-    goto exit_end;
-
-exit_end:
-    printf("Exit Thread Terminated\n");
-    pthread_exit(NULL);
 }
 
 /*****************************************
@@ -1240,7 +1178,6 @@ int32_t main(int32_t argc, char * argv[])
     /*Multithreading Variables*/
     int32_t create_thread_ai      = -1;
     int32_t create_thread_capture = -1;
-    int32_t create_thread_exit    = -1;
     int32_t create_thread_key     = -1;
     int32_t sem_create            = -1;
 
@@ -1430,17 +1367,6 @@ int32_t main(int32_t argc, char * argv[])
         ret_main = -1;
         goto end_threads;
     }
-
-    /*Create exit Thread*/
-    create_thread_exit = pthread_create(&exit_thread, NULL, R_exit_Thread, NULL);
-    if (0 != create_thread_exit)
-    {
-        fprintf(stderr, "[ERROR] Failed to create exit Thread.\n");
-        ret_main = -1;
-        goto end_threads;
-    }
-    /* Detached exit thread */
-    pthread_detach(exit_thread);
 
     /*Create Key Hit Thread*/
     create_thread_key = pthread_create(&kbhit_thread, NULL, R_Kbhit_Thread, NULL);
